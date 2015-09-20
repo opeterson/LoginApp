30d27a1f3abbfa408a87a3656e87662bde2886b6
opeterson <owen.a.peterson@gmail.com>
2015-09-20 08:19:13 -0500

    Added new LoginError model for displaying an error message on login failure. Added mappings in controller.

--- src/main/java/ca/owenpeterson/loginapp/controllers/LoginController.java
+++ src/main/java/ca/owenpeterson/loginapp/controllers/LoginController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
+import ca.owenpeterson.loginapp.models.LoginError;
 import ca.owenpeterson.loginapp.models.LoginForm;
 
 @Controller
@@ -18,23 +19,29 @@ public class LoginController {
 	
 	Logger logger = LogManager.getLogger(LoginController.class);
 
-	@RequestMapping(value="/login", method=RequestMethod.GET)
-	@ResponseStatus(HttpStatus.OK)
-	public @ResponseBody ModelAndView getView() {
-		logger.debug("LoginController: getView: Begin");
-		ModelAndView view = new ModelAndView("views/login/login.jsp");
-		logger.debug("LoginController: getView: End");
-		return view;
-	}
-	
 	@ModelAttribute("loginForm")
 	public LoginForm getForm() {
 		logger.debug("LoginController: getForm: Begin/End");
 		return new LoginForm();
 	}
 	
+	@ModelAttribute("errors")
+	public LoginError getErrors() {
+		logger.debug("LoginController: getErrors: Begin/End");
+		return new LoginError();
+	}
+		
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public @ResponseBody ModelAndView getView(@ModelAttribute("errors") LoginError errors) {
+		logger.debug("LoginController: getView: Begin");
+		ModelAndView view = new ModelAndView("views/login/login.jsp", "errors", errors);
+		logger.debug("LoginController: getView: End");
+		return view;
+	}
+	
 	@RequestMapping(value="/login", method=RequestMethod.POST)
-	public @ResponseBody ModelAndView doLogin(@ModelAttribute("loginForm") LoginForm loginForm) {
+	public @ResponseBody ModelAndView doLogin(@ModelAttribute("loginForm") LoginForm loginForm, @ModelAttribute("errors") LoginError errors) {
 		logger.debug("LoginController: doLogin: Begin");
 		logger.debug("LoginController: doLogin: User logged in with username: " + loginForm.getUsername());
 		logger.debug("LoginController: doLogin: User logged in with password: " + loginForm.getPassword());
---
+++ src/main/java/ca/owenpeterson/loginapp/models/LoginError.java
@@ -0,0 +1,29 @@
+package ca.owenpeterson.loginapp.models;
+
+public class LoginError {
+
+	private String errorMessage;
+	private String cssClass;
+			
+	public LoginError() {
+	}
+	public LoginError(String errorMessage, String cssClass) {
+		super();
+		this.errorMessage = errorMessage;
+		this.cssClass = cssClass;
+	}
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+	public String getCssClass() {
+		return cssClass;
+	}
+	public void setCssClass(String cssClass) {
+		this.cssClass = cssClass;
+	}
+	
+	
+}
