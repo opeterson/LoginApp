017f2f4d31dac575ca9f5de497de0e74a84f357a
opeterson <owen.a.peterson@gmail.com>
2015-09-20 09:07:15 -0500

    created AuthenticatedUser model which will be used to keep information about a user who has successfully logged in.

---
+++ src/main/java/ca/owenpeterson/loginapp/models/AuthenticatedUser.java
@@ -0,0 +1,33 @@
+package ca.owenpeterson.loginapp.models;
+
+import java.io.Serializable;
+
+public class AuthenticatedUser implements Serializable {
+
+	private static final long serialVersionUID = 2733978423374274037L;
+	private String username;
+	private String email;
+	
+	public AuthenticatedUser() {
+	}
+	
+	public AuthenticatedUser(String username, String email) {
+		this.username = username;
+		this.email = email;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
